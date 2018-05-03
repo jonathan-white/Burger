6cf7189fa3e4a305d5c7cbe2f1f943c1b901e643
Jon White <jon.white2@gmail.com>
2018-05-03 01:05:47 -0400

    JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 var mysql = require("mysql");
 var db = require("../config/db_conn");
+var connection;
 
 // Setup connection
-var connection = mysql.createConnection(db);
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection(db);
+}
 
 // Establish MySQL Connection
 connection.connect(function(err) {
